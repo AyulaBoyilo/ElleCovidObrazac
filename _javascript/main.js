fd7c5573d1efd25d1b6efcd36109ba1f6dc13698
DaliborA <d.arula@adriamedia.hr>
2021-11-08 21:24:14 +0100

    css and js

---
+++ _javascript/main.js
@@ -0,0 +1,67 @@
+// Anketa
+document.querySelector('#submit-form').addEventListener('click', function(e){
+
+  const url = 'https://script.google.com/macros/s/AKfycbzGDAs_pKaz6rsdG4UMQ7LRFUfSLBTD_W_bc8ePekD7DYOIMiK3YsGnRpqE3defLtqkug/exec';
+  
+
+  e.preventDefault();
+
+  if( document.getElementById('ime_prezime').value != '' &&
+      (document.getElementById('dolazim').checked || document.getElementById('ne_dolazim').checked) &&
+      (document.getElementById('DA_imam').checked || document.getElementById('NE_nemam').checked)
+  ) {
+
+      document.querySelector('#submit-form').classList.add('is-hidden');
+      document.querySelector('#error-msg').classList.add('is-hidden');
+      document.querySelector('#loading-indicator').classList.remove('is-hidden');
+
+      document.getElementById('ime_prezime').classList.remove('error_ime');
+      document.querySelector('.error_ime__message').classList.add('is-hidden');
+      document.querySelector('.error__labels__dolazak').classList.remove('error_dolazak');
+      document.querySelector('.error_dolazak__message').classList.add('is-hidden');
+      document.querySelector('.error__labels__potvrda').classList.remove('error_potvrda');
+      document.querySelector('.error_potvrda__message').classList.add('is-hidden');
+
+      fetch(url,{
+        method: 'POST',
+        mode: 'no-cors',
+        cache: 'no-cache',
+        headers: {
+        'Content-Type': 'application/json'
+        },
+        redirect: 'follow',
+        referrerPolicy: 'no-referrer',
+        body: $('form#ESA_obrazac').serializeJSON()
+      })
+        .then(document.querySelector('#loading-indicator').classList.add('is-hidden'))
+        .then(document.querySelector('#response-msg').classList.remove('is-hidden'));
+        
+  } else {
+    document.querySelector('#error-msg').classList.remove('is-hidden');
+
+    if (document.getElementById('ime_prezime').value == '') {
+      document.getElementById('ime_prezime').classList.add('error_ime');
+      document.querySelector('.error_ime__message').classList.remove('is-hidden');
+    } else {
+      document.getElementById('ime_prezime').classList.remove('error_ime');
+      document.querySelector('.error_ime__message').classList.add('is-hidden');
+    };
+
+    if (!(document.getElementById('dolazim').checked) && !(document.getElementById('ne_dolazim').checked)) {
+      document.querySelector('.error__labels__dolazak').classList.add('error_dolazak');
+      document.querySelector('.error_dolazak__message').classList.remove('is-hidden');
+    } else {
+      document.querySelector('.error__labels__dolazak').classList.remove('error_dolazak');
+      document.querySelector('.error_dolazak__message').classList.add('is-hidden');
+    };
+
+    if (!(document.getElementById('DA_imam').checked) && !(document.getElementById('NE_nemam').checked)) {
+      document.querySelector('.error__labels__potvrda').classList.add('error_potvrda');
+      document.querySelector('.error_potvrda__message').classList.remove('is-hidden');
+    } else {
+      document.querySelector('.error__labels__potvrda').classList.remove('error_potvrda');
+      document.querySelector('.error_potvrda__message').classList.add('is-hidden');
+    };
+  }
+
+});
